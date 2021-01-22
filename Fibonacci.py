8a0bf8e092f1e260cdc71d55a2e63278e731727a
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2021-01-22 16:47:37 -0300

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,14 +1,13 @@
 def Fibonacci(n):
-    nf=1
     n1=n2=1
-    if n==1 or n==2:
-        print("El número es 1")
+    if n<=2:
+        nf=1
     else:
         for i in range (n-2):
             nf=n1+n2
             n1=n2
             n2=nf
-    print(nf)
+    print("El número es:",nf)
 
 n=int(input("¿Qué índice de Fibonacci busca? "))
 Fibonacci(n)
