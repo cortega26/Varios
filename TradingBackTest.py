1a8fb3322ef8bf43db1c0f092293833e3cdc28ed
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2021-11-08 00:40:25 -0300

    Update TradingBackTest.py

--- TradingBackTest.py
+++ TradingBackTest.py
@@ -1,37 +1,34 @@
-import csv
+import pandas as pd
 import numpy as np
 
-#The csv file was downloaded from this URL: https://www.cryptodatadownload.com/cdd/Binance_BTCUSDT_minute.csv
-#The csv file order has been inverted: from oldest to newest in order to test it
-    
-with open(r'C:\Users\corte\Documents\Binance_BTCUSDT_minute.csv') as f: 
-    data = list(csv.reader(f, delimiter=","))
+filepath = "https://www.cryptodatadownload.com/cdd/Binance_BTCUSDT_minute.csv"
 
- 
-data = np.array(data)
-data = data[~np.all(data == '', axis=1)]
+import ssl  # we need to import this library and tweak one setting due to fact we use HTTPS certificate(s)
+ssl._create_default_https_context = ssl._create_unverified_context
 
-#dca: dollar cost-average
-#bos: Base Order Size
-#sos: Safety Order Size
-#crypto: amount of crypto invested
-#inv: amount invested
-#stc: safety trades count
-#tp: target profit %
-#ip: initial price
-#pd: price deviation to open safety orders (% from initial order)
-#low: lowest price of the inversal (minute)
-#high: highest price of the interval (minute)
+# Now we want to create a dataframe and use Pandas' to_csv function to read in our file
+data = pd.read_csv(filepath, skiprows=1)  # we use skiprows parameter because first row contains our web address
+data = np.array(data) #Convert the dataframe into an array in order to iterate thru it
+
+# LABELS
+# bos: base order size
+# sos: safety order size
+# inv: amount invested
+# stc: safety trades count
+# tp: target profit %
+# ip: initial price
+# pd: price deviation to open safety orders (% from initial order)
 
 def Backtest(tp,bos,sos,pd,stc):
     ip = 0 #initial price
     fp = 0 #Final profit for the Backtest Strategy
     stu = 0 #Safety trades used
-    for i in data:    
-        #print(i[6],i[5])
-        if float(i[6]) > 0 and float(i[5]) > 0:
-            low = float(i[5])
-            high = float(i[6])
+    #count = 0
+    for i in reversed(data):    
+        #print(i[5],i[6])
+        if float(i[5]) > 0 and float(i[6]) > 0:
+            low = float(i[5]) #highest price in the interval
+            high = float(i[6]) #lowest price in the interval
         else:
             print("Missing values or equal to zero. Program terminated.")
             break
@@ -40,23 +37,27 @@ def Backtest(tp,bos,sos,pd,stc):
             inv = bos
             dca = ip
             crypto = bos / ip
-        elif high >= dca * ((100 + tp)/100): #Condition to take profits
+        elif high >= dca * ((100 + tp) / 100): #Condition to take profits
             fp += tp/100 * inv
-            dca = dca * ((100 + tp)/100)
+            #print("take profit:",tp/100*inv)
+            dca = dca * ((100 + tp) / 100)
+            #print("New current DCA:",dca)
             inv = bos
             crypto = bos / dca
             stu = 0
-            ip = dca
-            #print(fp)
-        elif low <= ip * ((100 - pd*(1+stu))/100) and stu < stc: #Condition to activate safety trade
-            crypto += sos / (ip * ((100 - pd*(1+stu))/100))
+            ip = dca            
+        elif low <= ip * ((100 - pd * (1 + stu)) / 100) and stu < stc: #Condition to activate safety trade
+            crypto += sos / (ip * ((100 - pd * (1 + stu)) / 100))
             inv += sos
             stu += 1
             dca = inv / crypto
-            #print(dca)
         else: #in any other case do nothing
             pass
+    print("Safe Trades Used in current deal:",stu)
+    print("Money currently invested:",inv)
+    print("Current DCA:",round(dca,2))
     print("Total profit for the period:",round(fp,2))
     #print(count)
 
-Backtest(2,20,20,2,15)
+
+Backtest(3.26,10,10,2,30)
