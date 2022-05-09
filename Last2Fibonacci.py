28ffe31acd5c94f23357f1997c2899a260328d45
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-05-09 15:18:30 -0400

    Update Last2Fibonacci.py

--- Last2Fibonacci.py
+++ Last2Fibonacci.py
@@ -1,27 +1,27 @@
-# This code will give you the last 2 digits of any Fibonacci number in a fraction of a second
+# This code will give you the last 2 digits of any Fibonacci index
 
 def Fibonacci(n):
-    n1=n2=1
+    n1 = n2 = 1
     if n == 0:
         return 0
     elif n <= 2:
         return 1
     else:
         for i in range (n-2):
-            nf=n1+n2
-            n1=n2
-            n2=nf
+            nf = n1 + n2
+            n1 = n2
+            n2 = nf
     return nf
     
 def AdjustSize(n):
-    n=str(n)
+    n = str(n)
     for i in range(len(n)):
-        n=n[1:i]+n[i:]
+        n = n[1:i] + n[i:]
         if int(n) < 100:
             break
         else:
             pass
-    n=int(n)
+    n = int(n)
     return n
     
 def newFib(n):
