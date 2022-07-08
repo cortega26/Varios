6de17a3a12a819a7f6b5e2e8fd686033aee21190
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-07-08 13:05:12 -0400

    Update Geek-onacci.py

--- Geek-onacci.py
+++ Geek-onacci.py
@@ -1,16 +1,17 @@
-#Geek created a random series and given a name geek-onacci series.
-#Given four integers A, B, C, N. A, B, C represents the first three numbers of geek-onacci series.
-#Find the Nth number of the series.
-#The nth number of geek-onacci series is a sum of the last three numbers (summation of N-1th, N-2th, and N-3th geek-onacci numbers)
-#
-#Input:
-#1. The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
-#2. The first line of each test case contains four space-separated integers A, B, C, and N.
-#
-#Output: For each test case, print Nth geek-onacci number
+"""
+Geek created a random series and given a name geek-onacci series.
+Given four integers A, B, C, N. A, B, C represents the first three numbers of geek-onacci series.
+Find the Nth number of the series.
+The nth number of geek-onacci series is a sum of the last three numbers (summation of N-1th, N-2th, and N-3th geek-onacci numbers)
 
+Input:
+1. The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+2. The first line of each test case contains four space-separated integers A, B, C, and N.
 
-def Geekonacci(a: int, b: int, c: int, n: int):
+Output: For each test case, print Nth geek-onacci number
+"""
+
+def geekonacci(a: int, b: int, c: int, n: int):
     for j in range(n-3):
         geek = a + b + c
         a = b
@@ -18,4 +19,5 @@ def Geekonacci(a: int, b: int, c: int, n: int):
         c = geek
     return geek    
 
-
+if __name__ == "__main__":
+    geekonacci(5, 7, 9, 10)
