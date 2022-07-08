680cb7e8bc8b2801fb34c96f7e7622906c0765a4
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-07-08 13:02:25 -0400

    Update FractionToDecimal.py

--- FractionToDecimal.py
+++ FractionToDecimal.py
@@ -1,7 +1,7 @@
-def fractionToDecimal(numerator,denominator):
-    result=""
-    result+=str(numerator//denominator)
-    result+="."
+def fractionToDecimal(numerator, denominator):
+    result = ""
+    result += str(numerator // denominator)
+    result += "."
     quotient_num = []
     while numerator:
         remainder = numerator % denominator
@@ -9,11 +9,11 @@ def fractionToDecimal(numerator,denominator):
             for i in quotient_num:
                 result += str(i[-1])
             break
-        numerator = remainder*10
+        numerator = remainder * 10
         quotient = numerator // denominator
         if [numerator, quotient] not in quotient_num:
             quotient_num.append([numerator, quotient])
-        elif [numerator, quotient] in quotient_num:
+        else:
             index = quotient_num.index([numerator, quotient])
             for i in quotient_num[:index]:
                 result += str(i[-1])
@@ -22,8 +22,9 @@ def fractionToDecimal(numerator,denominator):
                 result += str(i[-1])
             result += ")"
             break
-    if result[-1:]==".":
-        result=result[:-1]
+    if result[-1:] == ".":
+        result = result[:-1]
     return result
 
-print(fractionToDecimal(1881,139))
+if __name__ == "__main__":
+    fractionToDecimal(1881, 139)
