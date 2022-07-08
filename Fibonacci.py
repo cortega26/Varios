486200cf67c9fb95c9b42cdfc6c4cbe73c481be3
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2022-07-08 12:59:01 -0400

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,13 +1,12 @@
-def Fibonacci(n):
-    n1=n2=1
-    if n<=2:
-        nf=1
-    else:
-        for i in range (n-2):
-            nf=n1+n2
-            n1=n2
-            n2=nf
-    print("El número es:",nf)
+def fibonacci(n):
+    a, b = 1, 1
+    if n <= 2:
+        return 1
+    for i in range (n-2):
+        nf = a + b
+        a = b
+        b = nf
+    return nf
 
-n=int(input("¿Qué índice de Fibonacci busca? "))
-Fibonacci(n)
+if __name__ == "__main__":
+    fibonacci(50)
